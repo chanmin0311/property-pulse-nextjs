e35593c863ecd1cc3fda607db93b152c035e3b25
chanmin0311 <ckstj1500@gmail.com>
2025-12-25 02:21:01 +0900

    create homepage, layout, style

---
+++ app/layout.jsx
@@ -0,0 +1,11 @@
+const MainLayout = ({ children }) => {
+    return (
+        <html lang="en">
+            <body>
+                <main>{children}</main>
+            </body>
+        </html>
+    );
+};
+
+export default MainLayout;
