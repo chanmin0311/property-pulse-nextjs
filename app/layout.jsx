4e336de7cb314dbf16d1a652f2150632dd64a882
chanmin0311 <ckstj1500@gmail.com>
2025-12-26 23:53:18 +0900

    add footer component to main layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,14 @@
 import "@/assets/styles/globals.css";
 import Navbar from "@/components/Navbar.jsx";
+import Footer from "@/components/Footer.jsx";
+
 const MainLayout = ({ children }) => {
     return (
         <html lang="en">
             <body>
                 <Navbar />
                 <main>{children}</main>
+                <Footer />
             </body>
         </html>
     );
