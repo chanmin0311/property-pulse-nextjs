0fcd3906a23f5ffe22ca6798eba943d84ef37109
chanmin0311 <ckstj1500@gmail.com>
2025-12-29 00:18:19 +0900

    feat: add fetching single property

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,4 +1,33 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import { fetchProperty } from "@/utils/requests";
+
 const PropertyPage = () => {
+    const { id } = useParams();
+
+    const [property, setProperty] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchPropertyData = async () => {
+            if (!id) return;
+            try {
+                const property = await fetchProperty(id);
+                setProperty(property);
+            } catch (error) {
+                console.error("Error Fetching Property: ", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        // otherwise, every time the property changes this will run never ending loop
+        if (property === null) {
+            fetchPropertyData();
+        }
+    }, [id, property]);
+
     return <div>PropertyPage</div>;
 };
 
