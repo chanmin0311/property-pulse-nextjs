39a2d6591e0153335a13a6c64895549a596269b4
chanmin0311 <ckstj1500@gmail.com>
2025-12-29 00:15:33 +0900

    refactor: extract data fetching logic into utils

--- app/properties/add/page.jsx
+++ app/properties/add/page.jsx
@@ -1,24 +1,8 @@
 import PropertyCard from "@/components/PropertyCard";
-
-// data fetching function
-async function fectchProperties() {
-    try {
-        const res = await fetch(
-            `${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`
-        );
-
-        if (!res.ok) {
-            throw new Error("Failed to fetch data");
-        }
-
-        return res.json();
-    } catch (error) {
-        console.log(error);
-    }
-}
+import { fetchProperties } from "@/utils/requests";
 
 const PropertiesPage = async () => {
-    const properties = await fectchProperties();
+    const properties = await fetchProperties();
 
     // sorting properties by updated date
     properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -1,25 +1,9 @@
 import PropertyCard from "@/components/PropertyCard";
 import Link from "next/link";
-
-// data fetching function
-async function fectchProperties() {
-    try {
-        const res = await fetch(
-            `${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`
-        );
-
-        if (!res.ok) {
-            throw new Error("Failed to fetch data");
-        }
-
-        return res.json();
-    } catch (error) {
-        console.log(error);
-    }
-}
+import { fetchProperties } from "@/utils/requests";
 
 const HomeProperties = async () => {
-    const properties = await fectchProperties();
+    const properties = await fetchProperties();
     const recentProperties = properties
         .sort(() => Math.random() - Math.random())
         .slice(0, 3);
