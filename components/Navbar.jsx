de5298b4809e624488a95210dffcd17268d7c593
chanmin0311 <ckstj1500@gmail.com>
2025-12-30 22:37:30 +0900

    feat(auth): add Google OAuth login based on NextAuth

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,13 +6,27 @@ import Link from "next/link";
 import logo from "@/assets/images/logo-white.png";
 import profileDefault from "@/assets/images/profile.png";
 import { usePathname } from "next/navigation";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 import { FaGoogle } from "react-icons/fa";
 
 const Navbar = () => {
+    const { data: session } = useSession();
+
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
     const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
-    const [isLoggedIn, setIsLoggedIn] = useState(true);
+    const [providers, setProviders] = useState(false);
+
     const pathname = usePathname();
+
+    useEffect(() => {
+        const setAuthProviders = async () => {
+            const res = await getProviders();
+            setProviders(res);
+        };
+
+        setAuthProviders();
+    }, []);
+    // console.log(providers);
 
     // closing the profile menu by clicking the other section of the page
     const profileMenuRef = useRef(null);
@@ -87,6 +101,7 @@ const Navbar = () => {
                                 className="h-10 w-auto"
                                 src={logo}
                                 alt="PropertyPulse"
+                                loading="eager"
                             />
 
                             <span className="hidden md:block text-white text-2xl font-bold ml-2">
@@ -116,7 +131,7 @@ const Navbar = () => {
                                 >
                                     Properties
                                 </Link>
-                                {isLoggedIn && (
+                                {session && (
                                     <Link
                                         href="/properties/add"
                                         className={`${
@@ -133,19 +148,30 @@ const Navbar = () => {
                     </div>
 
                     {/* <!-- Right Side Menu (Logged Out) --> */}
-                    {!isLoggedIn && (
+                    {!session && (
                         <div className="hidden md:block md:ml-6">
                             <div className="flex items-center">
-                                <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">
-                                    <FaGoogle className="text-white mr-2" />
-                                    <span>Login or Register</span>
-                                </button>
+                                {providers &&
+                                    Object.values(providers).map(
+                                        (provider, index) => (
+                                            <button
+                                                key={index}
+                                                onClick={() =>
+                                                    signIn(provider.id)
+                                                }
+                                                className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                                            >
+                                                <FaGoogle className="text-white mr-2" />
+                                                <span>Login or Register</span>
+                                            </button>
+                                        )
+                                    )}
                             </div>
                         </div>
                     )}
 
                     {/* <!-- Right Side Menu (Logged In) --> */}
-                    {isLoggedIn && (
+                    {session && (
                         <div className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
                             <Link href="/messages" className="relative group">
                                 <button
@@ -271,7 +297,7 @@ const Navbar = () => {
                         >
                             Properties
                         </Link>
-                        {isLoggedIn && (
+                        {session && (
                             <Link
                                 href="/properties/add"
                                 className={`${
@@ -284,12 +310,17 @@ const Navbar = () => {
                             </Link>
                         )}
 
-                        {!isLoggedIn && (
-                            <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5">
-                                <FaGoogle className="text-white mr-2" />
-                                <span>Login or Register</span>
-                            </button>
-                        )}
+                        {!session &&
+                            providers &&
+                            Object.values(providers).map((provider, index) => (
+                                <button
+                                    key={index}
+                                    onClick={() => signIn(provider.id)}
+                                    className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                                >
+                                    <span>Login or Regisger</span>
+                                </button>
+                            ))}
                     </div>
                 </div>
             )}
