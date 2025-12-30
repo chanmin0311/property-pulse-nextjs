a18625cb986c4c068fd3c02aaed3afe391d1fad4
chanmin0311 <ckstj1500@gmail.com>
2025-12-30 20:55:39 +0900

    feat(ui): implement Spinner for client side rendering

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -4,6 +4,7 @@ import { useParams } from "next/navigation";
 import { fetchProperty } from "@/utils/requests";
 import PropertyHeaderImage from "@/components/PropertyHeaderImage";
 import PropertyDetails from "@/components/PropertyDetails";
+import Spinner from "@/components/Spinner";
 import Link from "next/link";
 import { FaArrowLeft } from "react-icons/fa";
 
@@ -41,6 +42,7 @@ const PropertyPage = () => {
     }
     return (
         <>
+            {loading && <Spinner loading={loading} />}
             {!loading && property && (
                 <>
                     <PropertyHeaderImage image={property.images[0]} />
@@ -60,6 +62,7 @@ const PropertyPage = () => {
                         <div className="container m-auto py-10 px-6">
                             <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
                                 <PropertyDetails property={property} />
+
                                 <aside className="space-y-4">
                                     <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
                                         <i className="fas fa-bookmark mr-2"></i>{" "}
