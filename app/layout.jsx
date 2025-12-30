0c83ffa41bb73d9b538457d662031c185902ca76
chanmin0311 <ckstj1500@gmail.com>
2025-12-30 21:37:31 +0900

    feat: add meta data info

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,12 @@ import "@/assets/styles/globals.css";
 import Navbar from "@/components/Navbar.jsx";
 import Footer from "@/components/Footer.jsx";
 
+export const metadata = {
+    title: "PropertyPulse | Find The Perfect Rental",
+    description: "Find your dream rental property",
+    keywords: "rental, find rentals, find properties",
+};
+
 const MainLayout = ({ children }) => {
     return (
         <html lang="en">
