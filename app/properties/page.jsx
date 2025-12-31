cad6a7c64f09a17cf6bfeda0061a0e68a223ffad
chanmin0311 <ckstj1500@gmail.com>
2025-12-31 22:22:05 +0900

    feat(page): add /properties page

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,11 +1,29 @@
-import Link from "next/link";
+import PropertyCard from "@/components/PropertyCard";
+import { fetchProperties } from "@/utils/requests";
 
-const PropertiesPage = () => {
+const PropertiesPage = async () => {
+    const properties = await fetchProperties();
+
+    // sorting properties by updated date
+    properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
     return (
-        <div>
-            <h1>Property Page</h1>
-            <Link href="/">Go Home</Link>
-        </div>
+        <section className="px-4 py-6">
+            <div className="contailer-xl lg:container m-auto px-4">
+                {properties.length === 0 ? (
+                    <p>No Properties Found</p>
+                ) : (
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                        {properties.map((property) => (
+                            <PropertyCard
+                                key={property._id}
+                                property={property}
+                            />
+                        ))}
+                    </div>
+                )}
+            </div>
+        </section>
     );
 };
 
