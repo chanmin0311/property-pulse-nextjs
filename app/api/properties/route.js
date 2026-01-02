601fd1a03cff5ecf44d2bcaeaf099ab8cc34bb04
chanmin0311 <ckstj1500@gmail.com>
2026-01-02 22:09:28 +0900

    feat:  enable form data to database

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,5 +1,7 @@
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
+import { getSessionUser } from "@/utils/getSessionUser";
+import cloudinary from "@/config/cloudinary";
 
 // GET/api/properties
 export const GET = async (request) => {
@@ -14,5 +16,97 @@ export const GET = async (request) => {
     } catch (error) {
         console.log(error);
         return new Response("Something went wrong", { status: 500 });
+    }
+};
+
+// POST/api/properties
+export const POST = async (request) => {
+    try {
+        await connectDB();
+
+        const sessionUser = await getSessionUser();
+        if (!sessionUser || !sessionUser.user.id) {
+            return new Response("User ID is required", { status: 401 });
+        }
+
+        const { userId } = await sessionUser;
+
+        const formData = await request.formData();
+
+        // access all values from amenities and images
+        const amenities = formData.getAll("amenities");
+        const images = formData
+            .getAll("images")
+            .filter((image) => image.name !== "");
+
+        // create data object for database
+        const propertyData = {
+            name: formData.get("name"),
+            type: formData.get("type"),
+            description: formData.get("description"),
+            location: {
+                street: formData.get("location.street"),
+                city: formData.get("location.city"),
+                state: formData.get("location.state"),
+                zipcode: formData.get("location.zipcode"),
+            },
+            beds: formData.get("beds"),
+            baths: formData.get("baths"),
+            square_feet: formData.get("square_feet"),
+            amenities,
+            rates: {
+                weekly: formData.get("rates.weekly"),
+                monthly: formData.get("rates.monthly"),
+                nightly: formData.get("rates.nightly"),
+            },
+            seller_info: {
+                name: formData.get("seller_info.name"),
+                email: formData.get("seller_info.email"),
+                phone: formData.get("seller_info.phone"),
+            },
+            owner: userId,
+        };
+
+        // upload images to Clouduinary
+        const imageUploadPromises = [];
+
+        for (const image of images) {
+            // turning the image into an arrayBuffer
+            const imageBuffer = await image.arrayBuffer();
+            const imageArray = Array.from(new Uint8Array(imageBuffer));
+            const imageData = Buffer.from(imageArray);
+
+            // convet the image data to base64
+            const imageBase64 = imageData.toString("base64");
+
+            // make request to upload to cloudinary
+            const result = await cloudinary.uploader.upload(
+                `data:image/png;base64,${imageBase64}`,
+                {
+                    folder: "propertypulse",
+                }
+            );
+
+            imageUploadPromises.push(result.secure_url);
+
+            // wait for all images to upload
+            const uploadedImages = await Promise.all(imageUploadPromises);
+            // add uploaded image to the propertyData object
+            propertyData.images = uploadedImages;
+        }
+
+        const newProperty = new Property(propertyData);
+        await newProperty.save();
+
+        return Response.redirect(
+            `${process.env.NEXTAUTH_URL}/properties/${newProperty._id}`
+        );
+        // console.log(propertyData);
+
+        // return new Response(
+        //     JSON.stringify({ message: "Success" }, { status: 200 })
+        // );
+    } catch (error) {
+        new Response("Failed to add property", { status: 500 });
     }
 };
